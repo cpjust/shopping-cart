8f5152b250b6a94efaf6bb30d25c7accbad4fc45
Chris Just <cpjust@hotmail.com>
2021-03-31 09:33:45 -0400

    JUST-9: Changed to use SCSS.

--- src/app/breakpoints/breakpoints.component.ts
+++ src/app/breakpoints/breakpoints.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'cj-breakpoints',
   templateUrl: './breakpoints.component.html',
-  styleUrls: ['./breakpoints.component.css']
+  styleUrls: ['./breakpoints.component.scss']
 })
 export class BreakpointsComponent implements OnInit {
 
